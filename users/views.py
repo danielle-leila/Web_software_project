d5ab921b7c24d60d8caba250f26ccf670c6bf725
Valeriy <v.volodenkov>
2012-02-23 12:15:37 +0200

    changed default view

--- users/views.py
+++ users/views.py
@@ -9,8 +9,8 @@ def index(request):
     #return HttpResponse("Correct")
     #check if there is a user in db
     #user = User.objects.create_user('user','','user')
-    user = auth.authenticate(username='useru',password='user')
-    if user is not None:
-        return HttpResponse("Correct")
-    else:
-        return HttpResponse('not correct') 
+    #user = auth.authenticate(username='useru',password='user')
+    #if user is not None:
+    return HttpResponse("Correct")
+    #else:
+    #   return HttpResponse('not correct') 
